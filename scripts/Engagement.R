86d82ebea0228f31fda9cbfda52311ac4d4739a0
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2017-10-23 19:13:54 +0200

    Update Graphs
    Individual graphs for analysis updated

--- scripts/Engagement.R
+++ scripts/Engagement.R
@@ -10,39 +10,47 @@ table_answers = read.csv('data/Questionnaire_Answers.csv')
 answers_engagement <- table_answers[,75:86]
 
 likert_values <- c(-3,-2,-1,0,1,2,3)
-likert_labels <- c("Strongly disagree"," ","  ","Neutral","   ","    ","Strongly agree")
+likert_labels <- c("Very weak (-3)","(-2)","(-1)","Neutral (0)","(1)","(2)","Very strong (3)")
 likert_factors <- factor(likert_values)
 
 # Engagement with cycling
 eng1_before_factor <- factor(answers_engagement$engagement_A1, likert_factors,labels = likert_labels)
 eng1_after_factor <- factor(answers_engagement$engagement_B1, likert_factors,labels = likert_labels)
-
 eng1_factors <- data.frame(eng1_before_factor, eng1_after_factor)
 
-names(eng1_factors) <- c("With Cycling - Before", "With Cycling - After")
+names(eng1_factors) <- c("Before", "After")
+title <- "Indicate how weak or strong is your intention to use a bicycle"
 eng1_likert <- likert(eng1_factors)
-plot(eng1_likert)
+plot(eng1_likert, centered = TRUE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) + 
+  guides(fill=guide_legend(title=NULL, nrow = 1))
 
-# Engagement with mobile apps
+
+# Engagement with games for mobile phones
 eng2_before_factor <- factor(answers_engagement$engagement_A2, likert_factors,labels = likert_labels)
 eng2_after_factor <- factor(answers_engagement$engagement_B2, likert_factors,labels = likert_labels)
-
 eng2_factors <- data.frame(eng2_before_factor, eng2_after_factor)
 
-names(eng2_factors) <- c("With Mobile Games - Before", "With Mobile Games - After")
+title <- "Indicate how weak or strong is your intention to play some game on your phone"
+names(eng2_factors) <- c("Before", "After")
 eng2_likert <- likert(eng2_factors)
-plot(eng2_likert)
+plot(eng2_likert, centered = TRUE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) + 
+  guides(fill=guide_legend(title=NULL, nrow = 1))
 
 
 # Engagement with app for cycling
 eng3_before_factor <- factor(answers_engagement$engagement_A3, likert_factors,labels = likert_labels)
 eng3_after_factor <- factor(answers_engagement$engagement_B3, likert_factors,labels = likert_labels)
-
 eng3_factors <- data.frame(eng3_before_factor, eng3_after_factor)
 
-names(eng3_factors) <- c("With Aps for Cycling - Before", "With Aps for Cycling - After")
+title <- "Indicate how weak or strong is your intention to use an app while cycling"
+names(eng3_factors) <- c("Before", "After")
 eng3_likert <- likert(eng3_factors)
-plot(eng3_likert)
+plot(eng3_likert, centered = TRUE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) + 
+  guides(fill=guide_legend(title=NULL, nrow = 1))
+
+
+
+
 
 # Engagement 
 eng_factors <- data.frame(eng1_before_factor,eng1_after_factor, eng2_before_factor, eng2_after_factor, eng3_before_factor, eng3_after_factor)
@@ -58,13 +66,48 @@ dev.off()
 
 
 
+
+
 # Engagement with future cycling
 eng_future_cycling_factor <- factor(answers_engagement$engagement_cycling_2w, likert_factors,labels = likert_labels)
 eng_future_cycling2_factor <- factor(answers_engagement$engagement_cycling_future, likert_factors,labels = likert_labels)
+
+eng_future_cycling_ex <- data.frame(eng_future_cycling2_factor)
+eng_future_cycling_ex_likert <- likert(eng_future_cycling_ex)
+plot (eng_future_cycling_ex_likert)
+
+
+
+
+
+
+
+
+# Engagement with the experiment app
 eng_future_app_factor <- factor(answers_engagement$engagement_app_2w, likert_factors,labels = likert_labels)
 eng_future_app2_factor <- factor(answers_engagement$engagement_app_future, likert_factors,labels = likert_labels)
+
+eng_future_app_ex <- data.frame(eng_future_app_factor, eng_future_app2_factor)
+names(eng_future_app_ex) <- c("in the next 2 weeks", "In the future")
+title <- "I intend to use the app of the experiment"
+eng_future_app_likert <- likert(eng_future_app_ex)
+
+plot(eng_future_app_likert)
+plot(eng_future_app_likert, centered = TRUE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) +
+  guides(fill=guide_legend(title=NULL, nrow = 1))
+
+
+
+# Engagement with cycling apps
 eng_future_any_app_factor <- factor(answers_engagement$engagement_any_app_2w, likert_factors,labels = likert_labels)
 eng_future_any_app2_factor <- factor(answers_engagement$engagement_any_app_future, likert_factors,labels = likert_labels)
+
+eng_future_app_cycling <- data.frame(eng_future_any_app_factor, eng_future_any_app2_factor)
+eng_future_any_app_likert <- likert(eng_future_app_cycling)
+plot(eng_future_any_app_likert)
+
+
+
 
 
 eng_future_cycling <- data.frame(eng_future_cycling_factor, eng_future_cycling2_factor, eng_future_app_factor, eng_future_app2_factor, eng_future_any_app_factor, eng_future_any_app2_factor)
--- scripts/Satisfaction.R
+++ scripts/Satisfaction.R
@@ -8,22 +8,42 @@ require(likert)
 table_answers = read.csv('data/Questionnaire_Answers.csv')
 
 likert_values <- c(-3,-2,-1,0,1,2,3)
-likert_labels <- c("Strongly disagree"," ","  ","Neutral","   ","    ","Strongly agree")
+likert_labels <- c("Strongly disagree (-3)","(-2)","(-1)","Neutral (0)","(1)","(2)","Strongly agree (3)")
 likert_factors <- factor(likert_values)
 
 
-satis_before_factor <- factor(table_answers$profile_cycling_1, likert_factors, labels = likert_labels)
-satis_after_factor <- factor(table_answers$satisfaction_cycling, likert_factors, labels = likert_labels)
+# Satisfaction before and After
+satis_before_factor <- factor(table_answers$profile_cycling_1, likert_factors, labels = likert_labels )
+satis_after_factor <- factor(table_answers$satisfaction_cycling, likert_factors, labels = likert_labels )
 
-satis_factors <- data.frame(satis_before_factor, satis_after_factor)
+satis_factors <- data.frame( satis_after_factor, satis_before_factor)
 
-names(satis_factors) <- c("With Cycling - Before", "With Cycling - After")
+names(satis_factors) <- c("After", "Before")
+title <- "I would find cycling enjoyable'"
 satis_likert <- likert(satis_factors)
-plot(satis_likert)
+plot(satis_likert, centered = TRUE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) + 
+  guides(fill=guide_legend(title=NULL, nrow = 1))
+
 
 pdf("graphs/sastisfaction.pdf")
 plot(satis_likert)
 dev.off()
+
+
+# Satisfaction with cycling / Mobile App during the experiment 
+satis_cycling_during_ex_factor <- factor(table_answers$satisfaction_app_1, likert_factors, labels = likert_labels)
+satis_app_during_ex_factor <- factor(table_answers$satisfaction_app_2, likert_factors, labels = likert_labels)
+satis_cycling_during_ex <- data.frame(satis_app_during_ex_factor, satis_cycling_during_ex_factor)
+names
+title <- "How satisfied / dissatisfied during the experimetn you were with:"
+names(satis_cycling_during_ex) <- c("Cycling", "The Mobile App")
+satis_cycling_during_ex_likert <- likert(satis_cycling_during_ex)
+plot(satis_cycling_during_ex_likert, centered = FALSE) + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5)) + 
+  guides(fill=guide_legend(title=NULL, nrow = 1))
+
+
+
+# Satisfaction with the App during the experiment
 
 
 

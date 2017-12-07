0b4a7c5736b47ad29eea899252323b6baae2f7ff
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2017-12-07 18:51:06 +0100

    Firt attempt to use wordcloud

--- scripts/A_sentiment_polarity_tag.R
+++ scripts/A_sentiment_polarity_tag.R
@@ -10,28 +10,62 @@ table_tags <- read.csv('data/Cyclist_Tag.csv', sep = '\t')
 table_tags_polarity <- read.csv('data/Tags_polarity.csv')
 table_tags_sense_joined <- merge(table_tags, table_tags_polarity, all = TRUE)
 
-new_tags
+new_tags <- table_tags_sense_joined[is.na(table_tags_sense_joined$sentiment_polarity),]
+polarised_tags <- table_tags_sense_joined[!is.na(table_tags_sense_joined$sentiment_polarity),]
 
 
-
-docs <- Corpus( VectorSource( as.String(table_tags$text) ) )
-
+# Cloud 1 - All Tags
+docs <- Corpus( VectorSource( as.String(table_tags_sense_joined$text_en) ) )
 inspect(docs)
-
 toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
 docs <- tm_map(docs, toSpace, "/")
 docs <- tm_map(docs, toSpace, "@")
 docs <- tm_map(docs, toSpace, "\\|")
-
-
 dtm <- TermDocumentMatrix(docs)
 m <- as.matrix(dtm)
 v <- sort(rowSums(m),decreasing=TRUE)
 d <- data.frame(word = names(v),freq=v)
 head(d, 10)
-
-
 set.seed(1234)
+
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
+# Cloud 2 - New Tags
+docs <- Corpus( VectorSource( as.String(new_tags$text) ) )
+inspect(docs)
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+set.seed(1234)
+
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
+# Cloud 3 - Just Polarised Tags
+docs <- Corpus( VectorSource( as.String(polarised_tags$text) ) )
+inspect(docs)
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+set.seed(1234)
+
 wordcloud(words = d$word, freq = d$freq, min.freq = 1,
           max.words=200, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))

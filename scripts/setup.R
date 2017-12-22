2520add0598639dc039e5cb08cb419ec10ff4e06
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2017-12-22 19:00:38 +0100

    Attepnting to understand trip times and length

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,8 +1,18 @@
+# Description: This script loads the datasets needed for the analysis 
+# it sets variables based on data coming from the questionnaire and
+# the mobile application
+# It also creates the join those tables based on the participant id 
+#
+# Comments: set your working directory to the project folder
+# Author: Diego Pajarito 
+
 # Setup
-library(ggplot2)
-require(lubridate)
 
 table_participants = read.csv('data/Cyclist_Experiment.csv')
 table_trips = read.csv('data/Cyclist_Trip.csv', sep = '\t')
 table_answers = read.csv('data/Questionnaire_Answers.csv')
-trips_joined = merge(table_participants,table_trips)
+table_answers$group <- "none"
+table_answers[which(table_answers$competition_1 >= -3),]$group <- "Competition"
+table_answers[which(table_answers$collaboration_1 >= -3),]$group <- "Collaboration"
+trips_joined <- merge(table_participants,table_trips)
+trips_joined <- merge(trips_joined, table_answers)
